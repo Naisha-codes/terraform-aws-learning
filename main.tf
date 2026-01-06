e89da437b32b7b124b83f7a8110024d22163dbf1
Naisha-codes <n4naishakp@gmail.com>
2026-01-06 13:36:01 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -11,13 +11,34 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "${var.project_name}-bucket-demo"
+resource "aws_security_group" "ssh_access" {
+  name        = "terraform-ssh-access"
+  description = "Allow SSH access"
 
-  tags = {
-    Project = var.project_name
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
+
+  tags = {
+    Name = var.instance_name
+  }
+}
 
--- variables.tf
+++ variables.tf
@@ -1,12 +1,23 @@
 variable "region" {
-  description = "AWS region to deploy resources"
+  description = "AWS region"
   type        = string
   default     = "ap-south-1"
 }
 
-variable "project_name" {
-  description = "Project name for tagging"
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
   type        = string
-  default     = "terraform-recap"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "Name tag for EC2 instance"
+  type        = string
+  default     = "terraform-ec2-demo"
 }
 
